1c36db5dfbe84931af48c10c1748af8795c99c8f
Rashmi Sharmila <108237108+rashmisharmila@users.noreply.github.com>
2024-03-08 15:21:33 +0530

    builded the header layout

--- src/view/home.tsx
+++ src/view/home.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import Header from "../layout/header";
 function Home(): JSX.Element {
-  return (
-    <div>home</div>
+    return (
+        <div>
+            <section>
+                <Header />
+            </section>
+        </div>
     );
 
 }
